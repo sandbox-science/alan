934dee777007f871f8dc944ada65da606152f4e6
Chris <rolletchris1@gmail.com>
2025-05-23 21:49:27 -0700

    Add bot implementation in alan.py and restructure setup.py

---
+++ bot/alan.py
@@ -0,0 +1,53 @@
+import time
+import os
+
+from simplematrixbotlib import Bot, Creds, MessageMatch
+from config.config import load_config
+from bot.handlers import echo, ping, greeting, react
+from nio import RoomMemberEvent
+import asyncio
+
+def setup_bot():
+    cfg   = load_config()
+    creds = Creds(cfg["CLIENT"], cfg["USERNAME"], cfg["PASSWORD"])
+
+    return Bot(creds), cfg["PREFIX"]
+
+def setup_handlers(bot, prefix):
+    """
+    Setup the bot handlers for different events.
+    """
+    @bot.listener.on_message_event
+    async def _on_message(room, event):
+        latency_start = time.monotonic()
+        match         = MessageMatch(room, event, bot, prefix)
+        if match.is_not_from_this_bot() and match.prefix():
+            await ping.on_ping(match, bot, latency_start)
+            await echo.on_echo(match, bot)
+            await greeting.on_greeting(match, bot)
+        else:
+            await react.on_react(room, event, bot)
+
+    @bot.listener.on_custom_event(RoomMemberEvent)
+    async def _on_member(room, event):
+        if event.membership == "join" and \
+           event.source.get("unsigned", {}).get("prev_content", {}).get("membership") != "join":
+            await greeting.on_join(event, room, bot)
+
+def start():
+    """
+    Entry point for the bot.
+    """
+    # Check if the config.yaml file exists
+    if not os.path.exists('config/config.yaml'):
+        print("config.yaml file not found. Please create one.")
+        return
+
+    # Load the configuration and create the bot instance
+    bot, prefix = setup_bot()
+    setup_handlers(bot, prefix)
+
+    bot.run()
+
+# Run the main function in an asyncio event loop
+asyncio.get_event_loop().run_until_complete(start())
--- setup.py
+++ setup.py
@@ -1,55 +1,18 @@
-import time
-import os
+from setuptools import setup, find_packages
 
-from simplematrixbotlib import Bot, Creds, MessageMatch
-from config import load_config
-from handlers import echo, ping, greeting, react
-from nio import RoomMemberEvent
-
-
-def setup_bot():
-    cfg   = load_config()
-    creds = Creds(cfg["CLIENT"], cfg["USERNAME"], cfg["PASSWORD"])
-
-    return Bot(creds), cfg["PREFIX"]
-
-def setup_handlers(bot, prefix):
-    """
-    Setup the bot handlers for different events.
-    """
-    @bot.listener.on_message_event
-    async def _on_message(room, event):
-        latency_start = time.monotonic()
-        match         = MessageMatch(room, event, bot, prefix)
-        if match.is_not_from_this_bot() and match.prefix():
-            await ping.on_ping(match, bot, latency_start)
-            await echo.on_echo(match, bot)
-            await greeting.on_greeting(match, bot)
-        else:
-            await react.on_react(room, event, bot)
-
-    @bot.listener.on_custom_event(RoomMemberEvent)
-    async def _on_member(room, event):
-        if event.membership == "join" and \
-           event.source.get("unsigned", {}).get("prev_content", {}).get("membership") != "join":
-            await greeting.on_join(event, room, bot)
-
-def main():
-    """
-    Entry point for the bot.
-    """
-    # Check if the config.yaml file exists
-    if not os.path.exists('config.yaml'):
-        print("config.yaml file not found. Please create one.")
-        return
-
-    os.system('pip3 install -r requirements.txt') # Install dependencies
-
-    # Load the configuration and create the bot instance
-    bot, prefix = setup_bot()
-    setup_handlers(bot, prefix)
-
-    bot.run()
-
-if __name__ == "__main__":
-    main()
+setup(
+    name='Alan',
+    version='1.0.0',
+    author='Sandbox Science',
+    description='The Sandbox Science Matrix community Bot',
+    url="https://github.com/sandbox-science/alan",
+    # license="GNU General Public License v3.0",
+    python_requires='>=3.10.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'simplematrixbotlib>=2.12.3',
+        'PyYAML>=6.0',
+    ],
+    scripts=['bin/alan'],
+)
