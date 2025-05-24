06da33488cbadbf982cd82085108a98a8075b97f
Chris <rolletchris1@gmail.com>
2025-05-23 21:49:04 -0700

    [Refactor] Refactor the structure of the project

--- config/config.py
+++ config/config.py
@@ -4,6 +4,6 @@ def load_config():
     """
     Load the configuration file.
     """
-    with open("config.yaml", "r") as f:
+    with open("config/config.yaml", "r") as f:
         cfg = yaml.safe_load(f)
     return cfg
